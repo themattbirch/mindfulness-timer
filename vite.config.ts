b7b4b666a97f1cd933e2a1f7eae4b20af8426f73
Matt B <matt@optimizewebsolutions.com>
2024-12-05 20:35:17 -0500

    moved

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,26 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 
+// Vite configuration
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        background: resolve(__dirname, 'src/background.ts')
+        background: resolve(__dirname, 'src/background/background.ts'), // Updated path
       },
       output: {
-        entryFileNames: '[name].js'
-      }
-    }
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js',
+        assetFileNames: '[name].[ext]',
+      },
+    },
+    outDir: 'dist',
+    emptyOutDir: true,
   },
-  publicDir: 'public'
-}); 
+  esbuild: {
+    logOverride: { 'module level directives cause errors': 'silent' }, // Suppress 'use client' warnings
+  },
+  publicDir: 'public',
+});
